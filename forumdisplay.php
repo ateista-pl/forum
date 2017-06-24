5ab127b5ab28278acd28b578604b3f8dfbc73007
Bartłomiej Kamiński <fizyk20@gmail.com>
2017-06-25 01:14:01 +0200

    Poprawa liczenia stron

--- forumdisplay.php
+++ forumdisplay.php
@@ -1122,7 +1122,7 @@ if(!empty($threadcache) && is_array($threadcache))
 		$morelink = '';
 		$thread['posts'] = $thread['replies'] + 1;
 
-		if($thread['unapprovedposts'] > 0 && $ismod)
+		if($ismod)
 		{
 			$thread['posts'] += $thread['unapprovedposts'] + $thread['deletedposts'];
 		}
